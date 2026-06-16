93a8ffc924d69d072a92039402d3f2517c2d8fb6
Bohdan <SimBa5762@users.noreply.github.com>
2026-06-16 09:29:43 +0300

    Додано нову папку та локально видалено файли

--- db/migrate_delivered_at.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE orders ADD COLUMN IF NOT EXISTS delivered_at TIMESTAMP;
-
-UPDATE orders o
-SET delivered_at = o.created_at
-FROM statuses s
-WHERE o.status_id = s.id
-  AND s.name = 'delivered'
-  AND o.delivered_at IS NULL;
